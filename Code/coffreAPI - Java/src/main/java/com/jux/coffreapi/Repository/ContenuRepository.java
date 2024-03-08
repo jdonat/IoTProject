0344f770cd94c241aaaac18c81fb597c96502ad2
jdonat <j.donatmagnin@it-students.fr>
2024-03-08 13:13:22 +0100

    Release V1

--- Code/coffreAPI - Java/src/main/java/com/jux/coffreapi/Repository/ContenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jux.coffreapi.Repository;
-
-import com.jux.coffreapi.Model.Contenu;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContenuRepository extends CrudRepository<Contenu, Integer> {
-}
--- Code/coffreAPI - Java/src/main/java/com/jux/coffreapi/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jux.coffreapi.Repository;
-
-import com.jux.coffreapi.Model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
